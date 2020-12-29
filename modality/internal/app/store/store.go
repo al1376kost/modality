21fc35bd480b30c745ba9493394e328a02cbadbf
aleksei.kostyuchenko <aleksei.kostyuchenko@becloud.by>
2020-12-29 11:53:25 +0300

    delete folder((

--- modality/internal/app/store/store.go
+++
@@ -1,6 +0,0 @@
-package store
-
-// Store ...
-type Store interface {
-	Modality() ModalityRepository
-}
--- modality/migrations/alter.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE modalities RENAME COLUMN start_date_time TO add_date_time;
-ALTER TABLE input_texts ADD COLUMN url TEXT NOT NULL DEFAULT '';
